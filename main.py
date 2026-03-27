9a2f5ccb0941709a935207ca4b96617f277eec63
Jonathan Lalande <lalandej.gg@gmail.com>
2026-03-26 21:34:10 -0400

    feat: hydrate Aircraft instance with data from sqlite

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import json
+import sqlite3
 
 from aircraft import Aircraft
 
 
+def load_aircraft_data_by_icao(conn: sqlite3.Connection, icao: str) -> dict:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM aircrafts WHERE icao = ?", (icao.upper(),))
 
+    aircraft = {}
+    for row in cursor.fetchall():
+        icao, registration, aircraft_type, name = row
+        aircraft["icao"] = icao
+        aircraft["registration"] = registration
+        aircraft["type"] = aircraft_type
+        aircraft["name"] = name
 
+    return aircraft
 
 
 with open("/tmp/aircraft.json", "r") as f:
     data = json.loads(f.read())
     aircrafts: dict[str, Aircraft] = {}
+
+    db_conn = sqlite3.connect("db.sqlite")
 
     for aircraft_data in data["aircraft"]:
         icao = aircraft_data["hex"]
@@ -17,12 +31,16 @@ with open("/tmp/aircraft.json", "r") as f:
         if not icao:
             pass
 
-        aircraft = Aircraft(aircraft_data)
+        db_data = load_aircraft_data_by_icao(db_conn, icao)
+        aircraft = Aircraft(aircraft_data, db_data)
 
         print("------------------------------")
         print("ICAO: ", aircraft.icao_hex())
+        print("SQUAWK: ", aircraft.squawk())
+        print("NAME: ", aircraft.name())
+        print("TYPE: ", aircraft.aircraft_type())
         print("FLIGHT: ", aircraft.flight())
         print("ALTITUDE: ", aircraft.altitude())
-        print("SQUAWK: ", aircraft.squawk())
+        print("REGISTRATION: ", aircraft.registration())
 
         aircrafts[aircraft.icao_hex()] = aircraft
